f9732ebd70b2b1ea975816b7c3916aada69b363f
seungmanlee <kyww1119@gmail.com>
2023-04-27 22:51:42 +0900

    - 컨트롤러 코드 수정 - jpa 설정 추가 & datasource 메모리로 변경

--- src/main/java/doit/dodo/controller/TodoController.java
+++ src/main/java/doit/dodo/controller/TodoController.java
@@ -4,8 +4,10 @@ import doit.dodo.model.TodoDTO;
 import doit.dodo.model.TodoRequest;
 import doit.dodo.model.TodoResponse;
 import doit.dodo.service.TodoService;
+
 import java.util.List;
 import java.util.stream.Collectors;
+
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,46 +22,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/todos")
 public class TodoController {
-  private final TodoService todoService;
-  private final TodoResponse todoResponse;
+    private final TodoService todoService;
 
-  @GetMapping
-  public List<TodoResponse> retrieve() {
-    List<TodoDTO> alltodolist = todoService.getAllTodoList();
-    List<TodoResponse> todoResponse = alltodolist.stream().map(all ->
-        new TodoResponse(all.getId(), all.getContent(), all.getMemo(),
-            all.getCreatedDate(), all.getUpdatedDate())).collect(Collectors.toList());
-    return todoResponse;
-  }
+    @GetMapping
+    public List<TodoResponse> retrieve() {
+        List<TodoDTO> allTodoList = todoService.getAllTodoList();
+        return allTodoList.stream().map(TodoController::convert).collect(Collectors.toList());
+    }
 
-  @GetMapping("/{id}")
-  public TodoResponse getTodo(@PathVariable Long id) {
-    TodoDTO todo = todoService.getTodo(id);
-    return todoResponse;
-  }
 
-  @PostMapping
-  public TodoResponse createTodo(@RequestBody TodoRequest todoRequest) {
-    TodoDTO todoDTO = new TodoDTO();
-    todoDTO.setContent(todoRequest.getContent());
-    todoDTO.setMemo(todoRequest.getMemo());
-    return todoResponse(todoService.createTodo(todoDTO));
-  }
+    @GetMapping("/{id}")
+    public TodoResponse getTodo(@PathVariable Long id) {
+        TodoDTO todo = todoService.getTodo(id);
+        return convert(todo);
+    }
 
-  private TodoResponse todoResponse(TodoDTO todo) {
-    return todoResponse(todo);
-  }
+    @PostMapping
+    public TodoResponse createTodo(@RequestBody TodoRequest todoRequest) {
+        TodoDTO todoDTO = new TodoDTO();
+        todoDTO.setContent(todoRequest.getContent());
+        todoDTO.setMemo(todoRequest.getMemo());
+        return convert(todoService.createTodo(todoDTO));
+    }
 
-  @PutMapping("/{id}")
-  public TodoResponse modifyTodo(@PathVariable Long id, @RequestBody TodoRequest todoRequest) {
-    TodoDTO todo = todoService.getTodo(id);
-    todo.setContent(todoRequest.getContent());
-    todo.setMemo(todoRequest.getMemo());
-    return todoResponse(todoService.updateTodo(id,todo));
-  }
+    @PutMapping("/{id}")
+    public TodoResponse modifyTodo(@PathVariable Long id, @RequestBody TodoRequest todoRequest) {
+        TodoDTO todo = todoService.getTodo(id);
+        todo.setContent(todoRequest.getContent());
+        todo.setMemo(todoRequest.getMemo());
+        return convert(todoService.updateTodo(id, todo));
+    }
 
-  @DeleteMapping("/{id}")
-  public void deleteTodo(@PathVariable Long id) {
-    todoService.deleteTodo(id);
-  }
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable Long id) {
+        todoService.deleteTodo(id);
+    }
+
+    private static TodoResponse convert(TodoDTO dto) {
+        return new TodoResponse(dto.getId(), dto.getContent(), dto.getMemo(),
+                dto.getCreatedDate(), dto.getUpdatedDate());
+    }
 }
